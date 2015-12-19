10180a8c81afbf55b84882426f033e9fb771bf67
spv <stellavalcheva@yahoo.com>
2015-12-20 00:57:48 +0200

    Added toast notification

--- UpgradeYourself.App/UpgradeYourself.Windows/Views/NavigationBar.xaml.cs
+++ UpgradeYourself.App/UpgradeYourself.Windows/Views/NavigationBar.xaml.cs
@@ -15,6 +15,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Notifications;
+using Windows.UI.Popups;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -31,6 +33,26 @@ namespace UpgradeYourself.Windows.Views
         {
             ParseUser.LogOut();
             (Window.Current.Content as Frame).Navigate(typeof(LogInPage));
+        }
+
+        private async void OnReminderButtonClicked(object sender, RoutedEventArgs e)
+        {
+            var notifier = ToastNotificationManager.CreateToastNotifier();
+
+            if (notifier.Setting != NotificationSetting.Enabled)
+            {
+                var dialog = new MessageDialog("Notifications are not enabled!");
+                await dialog.ShowAsync();
+                return;
+            }
+
+            var template = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+            var element = template.GetElementsByTagName("text")[0];
+            element.AppendChild(template.CreateTextNode("Practice regularly to stay fit!"));
+
+            var date = DateTimeOffset.Now.AddSeconds(15);
+            var stn = new ScheduledToastNotification(template, date);
+            notifier.AddToSchedule(stn);
         }
     }
 }
